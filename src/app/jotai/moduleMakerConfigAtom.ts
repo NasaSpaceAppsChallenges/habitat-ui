add6d7d33cd3794f14aaa1eaebfabba8750788a1
Jefferson Lima Dos Santos <jefferson.santos@sebsa.com.br>
2025-10-04 14:19:30 -0300

    atomo dos modulos

--- src/app/jotai/moduleMakerConfigAtom.ts
+++ src/app/jotai/moduleMakerConfigAtom.ts
@@ -1,23 +1,53 @@
 import { atom } from "jotai";
 
-export const moduleMakerConfigAtom = atom([
-  {
-    floors: [
-      {
-        level: 1,
-        x: 12,
-        y: 8,
-      },
-      {
-        level: 2,
-        x: 8,
-        y: 8,
-      },
-      {
-        level: 3,
-        x: 6,
-        y: 10,
-      },
-    ]
+export type HabitatFloors = {
+  level: Number;
+  x_length: Number;
+  y_length: Number;
+}
+export type ModuleTypes =
+  | 'kitchen'
+  | 'living_room'
+  | 'bedroom'
+  | 'bathroom'
+  | 'laboratory'
+  | 'workshop'
+  | 'storage'
+  | 'communications'
+  | 'life_support'
+  | 'exercise'
+  | 'medical'
+  | 'common_area'
+  | String;
+
+export type HabitatModules = {
+  uuid: String;
+  name: String;
+  reason: String;
+  type: ModuleTypes;
+  relationship_with: {
+    type: ModuleTypes;
+    points: Number;
+    reason: String;
   }
-]);
+}
+
+export type ModuleMissionMakerResponse = {
+  name: String;
+  description: String;
+  duration: String;
+  crewSize: Number;
+  habitat_floors: HabitatFloors[];
+  habitat_modules: HabitatModules[];
+}
+
+export const ModuleMakerConfigAtom = atom<ModuleMissionMakerResponse>(
+  {
+    name: '',
+    description: '',
+    duration: '',
+    crewSize: 0,
+    habitat_floors: [],
+    habitat_modules: []
+  }
+)
